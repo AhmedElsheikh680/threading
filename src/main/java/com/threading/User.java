7c5c2b0e1e158add19f9e734158681fa5aeefc4f
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-04-23 23:45:02 +0200

    impl thread one Closes #1

--- src/main/java/com/threading/User.java
+++ src/main/java/com/threading/User.java
@@ -1,4 +1,21 @@
 package com.threading;
 
-public class User {
+public class User extends Thread {
+
+    private String value;
+
+    public User(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public void run() {
+        this.print();
+    }
+
+    public void print(){
+        for(int i=0; i<5; i++){
+            System.out.println(value);
+        }
+    }
 }
